35271f73a8ec8417df6c2f05e4d8bf65fac5971c
Ghislain LEVEQUE <ghislain.leveque@clarisys.fr>
2012-06-26 17:16:44 +0200

    Supprimer les accents

--- domisolo.py
+++ domisolo.py
@@ -12,73 +12,73 @@ Introduction
 ============
 
 Bienvenue dans cette aide de jeu pour Dominion permettant de jouer en
-solitaire. Cette variante a été créée par Bastien Nemett et est disponible ici:
+solitaire. Cette variante a ete creee par Bastien Nemett et est disponible ici:
 http://www.trictrac.net/jeux/forum/viewtopic.php?p=1334034
 
-Le but du jeu est d'obtenir, en 20 tours de jeu, plus de point qu'une province
-choisie en début de partie (les cartes "Conquête")
+Le but du jeu est d'obtenir, en 20 tours de jeu, plus de point q'une province
+choisie en debut de partie (les cartes "Conquete")
 
-Sélectionnez de la façon qui vous plaira les 10 cartes royaumes avec lesquelles
+Selectionnez de la facon qui vous plaira les 10 cartes royaumes avec lesquelles
 vous jouerez.
 
-Au début de chaque tour de jeu, un évènement vous sera présenté. Celui-ci
-influencera vos différentes phases du jeu.
+Au debut de chaque tour de jeu, un evenement vous sera presente. Celui-ci
+influencera vos differentes phases du jeu.
 
 A la fin de chaque tour de jeu, le temps passe. Vous avez 20 tours avant la fin
 de la partie.
 
-Quelques ajustement des cartes Action/Attaque et Action/Réaction
+Quelques ajustement des cartes Action/Attaque et Action/Reaction
 ================================================================
 
-Sorcière : Piochez 2 cartes + Posez une carte malédiction sur la carte
-"Conquête" que vous avez choisie
+Sorciere : Piochez 2 cartes + Posez une carte malediction sur la carte
+"Conquete" que vous avez choisie
 
-Milice : +2 pièces / Avancez le marqueur "Armée" d'une case sur la carte
-conquête
+Milice : +2 pieces / Avancez le marqueur "Armee" d'une case sur la carte
+conquete
 
-Voleur : Prenez 1 cuivre / Avancez le marqueur "Trésor" d'une case sur la carte
-conquête
+Voleur : Prenez 1 cuivre / Avancez le marqueur "Tresor" d'une case sur la carte
+conquete
 
 Espion : Regardez la carte du sommet de votre deck. Choisissez si vous la
-défaussez ou non + Avancez le marqueur "Armée" d'une case sur la carte conquête
+defaussez ou non + Avancez le marqueur "Armee" d'une case sur la carte conquete
 
-Bureaucrate : Recevez 1 argent / Avancez le marqueur "Trésor" d'une case sur la
-carte conquête
+Bureaucrate : Recevez 1 argent / Avancez le marqueur "Tresor" d'une case sur la
+carte conquete
 
-A chaque fois qu'un marqueur "Armée" ou "Trésor" atteint la dernière case de sa
-piste sur la carte "conquête", posez une carte malédiction sur la carte
-conquête et remettez le marqueur au début de sa piste. Pour ce point de règle,
-vous n'avez rien à faire, c'est le logiciel qui s'en occupe.
+A chaque fois q'un marqueur "Armee" ou "Tresor" atteint la derniere case de sa
+piste sur la carte "conquete", posez une carte malediction sur la carte
+conquete et remettez le marqueur au debut de sa piste. Pour ce point de regle,
+vous n'avez rien a faire, c'est le logiciel qui s'en occupe.
 
 ###############################################################################
 """
 
 CONQUESTS = [
-    (u'Domaine', 20, 5),
-    (u'Duché', 30, 5),
-    (u'Province', 30, 7),
-    (u'Royaume', 40, 5),
-    (u'Empire', 40, 7),
+    ('Domaine', 20, 5),
+    ('Duche', 30, 5),
+    ('Province', 30, 7),
+    ('Royaume', 40, 5),
+    ('Empire', 40, 7),
     ]
 
 class DomiSolo(object):
 
     DEFAULT_EVENTS = [
-        (u"Architecte", u"Achat d'un bâtiment : coût -1 pièce."),
-        (u"Banditisme", u"Dévoilez les 2 premières cartes de votre deck. Si vous dévoilez une carte trésor, écartez là, si vous en révélez 2, écartez la plus élevée. Défaussez l'autre carte."),
-        (u"Banque", u" Vous pouvez écarter autant de cartes Trésors de votre que main que vous le souhaitez, recevez ensuite les cartes Trésors de votre choix d'une valeur totale inférieure ou égale au coût des cartes écartées, ajouter ces cartes à votre main."),
-        (u"Colporteur", u"+1 achat (pour +1 pièce)"),
-        (u"Emeutes", u"Prenez une carte malédiction"),
-        (u"Impôt Royal", u"-Phase d'achat- Vous ne pouvez dépenser que la moitié (arrondie à l'entier supérieur) de vos pièces disponibles (Trésor et Action)"),
-        (u"Inquisition", u"-Phase action- Les cartes Sorcière, Laboratoire, Festin et Bibliothèque sont interdites / -Phase achat- Les cartes Chapelle, Milice, Chancelier et Bureaucrate coûtent -1 pièce"),
-        (u"Invasion", u"Défaussez vous de cartes jusqu'à n'en avoir plus que 3 en main"),
-        (u"Peste", u"-Phase d'action- Vous ne pouvez poser aucune carte Personnage"),
-        (u"Saison Faste", u"+2 pièces"),
-        (u"Sécheresse", u"Ne piochez que 3 cartes lors de la phase d'ajustement"),
-        (u"Tremblement de terre", u"-Phase d'action- Vous ne pouvez poser aucune carte Bâtiment"),
+        ("Architecte", "Achat d'un batiment : cout -1 piece."),
+        ("Banditisme", "Devoilez les 2 premieres cartes de votre deck. Si vous devoilez une carte tresor, ecartez la, si vous en revelez 2, ecartez la plus elevee. Defaussez l'autre carte."),
+        ("Banque", " Vous pouvez ecarter autant de cartes Tresors de votre que main que vous le souhaitez, recevez ensuite les cartes Tresors de votre choix d'une valeur totale inferieure ou egale au cout des cartes ecartees, ajouter ces cartes a votre main."),
+        ("Colporteur", "+1 achat (pour +1 piece)"),
+        ("Emeutes", "Prenez une carte malediction"),
+        ("Impot Royal", "-Phase d'achat- Vous ne pouvez depenser que la moitie (arrondie a l'entier superieur) de vos pieces disponibles (Tresor et Action)"),
+        ("Inquisition", "-Phase action- Les cartes Sorciere, Laboratoire, Festin et Bibliotheque sont interdites / -Phase achat- Les cartes Chapelle, Milice, Chancelier et Bureaucrate coutent -1 piece"),
+        ("Invasion", "Defaussez vous de cartes jusq'a n'en avoir plus que 3 en main"),
+        ("Peste", "-Phase d'action- Vous ne pouvez poser aucune carte Personnage"),
+        ("Saison Faste", "+2 pieces"),
+        ("Secheresse", "Ne piochez que 3 cartes lors de la phase d'ajustement"),
+        ("Tremblement de terre", "-Phase d'action- Vous ne pouvez poser aucune carte Batiment"),
         ]
 
-    SEASONS = ['Printemps', 'été', 'automne', 'hiver']
+    SEASONS = ['Printemps', 'ete', 'automne', 'hiver']
 
     MAX_YEAR = 5
 
@@ -103,12 +103,12 @@ class DomiSolo(object):
 
     def gameTurn(self):
         event_name, event_desc = self.events.pop()
-        print u"Tour", self.current_turn, u"(", self.SEASONS[self.season], u"année", self.year,u") Marqueurs : Armée", self.army,u"- Tresor",self.treasure,u"/ Objectif",self.victory_target - self.curse
+        print "Tour", self.current_turn, "(", self.SEASONS[self.season], "annee", self.year,") Marqueurs : Armee", self.army,"- Tresor",self.treasure,"/ Objectif",self.victory_target - self.curse
         print event_name,":",event_desc
         #print "Vous pouvez effeectuer votre action et votre achat, en respectant les consignes ci-dessus. Defaussez ensuite toutes les cartes et reconstituez-vous une main de 5 cartes"
         answer = None
         while answer is None or answer in ('a','t','m','h'):
-            print u"[a: marqueur armée (Milice ou Espion)/t: marqueur trésor (Voleur ou Bureaucrate)/m: malédiction (Sorcière)/q: quitter/Entrée: fin du tour/h: help]"
+            print "[a: marqueur armee (Milice ou Espion)/t: marqueur tresor (Voleur ou Bureaucrate)/m: malediction (Sorciere)/q: quitter/Entree: fin du tour/h: help]"
             answer = raw_input()
             if answer == "a":
                 self.army += 1
@@ -131,12 +131,12 @@ class DomiSolo(object):
             self.season = 0
             self.year += 1
             if self.year == self.MAX_YEAR:
-                print u"Jeu terminé... Objectif a atteindre : ", self.victory_target - self.curse
+                print "Jeu termine... Objectif a atteindre : ", self.victory_target - self.curse
                 self.running = False
             elif self.year % 2 == 0:
-                print u"On mélange les évènements"
+                print "On melange les evenements"
                 self.events = self.shuffleArray(self.DEFAULT_EVENTS)
-        print u"Le temps passe..."
+        print "Le temps passe..."
         print
         print
 
@@ -151,12 +151,12 @@ class TextUI(object):
     def chooseConquest(self):
         answer = None
         while answer not in ('1','2','3','4','5','q'):
-            print u"""Choisissez votre conquête (difficulté):
+            print """Choisissez votre conquete (difficulte):
     %s
 
     q - Quitter
     """ % ("""
-    """.join([u"%d - %s - %d points - %d cases""" % (i+1, c[0], c[1], c[2]) for i, c in enumerate(CONQUESTS)]),)
+    """.join(["%d - %s - %d points - %d cases""" % (i+1, c[0], c[1], c[2]) for i, c in enumerate(CONQUESTS)]),)
             answer = raw_input()
         if answer == 'q':
             return None
